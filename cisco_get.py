2b34c20fb11e37d892c2f854e9f7eeba5c4ba78f
swordcrash <510628166@qq.com>
2018-11-26 11:04:42 +0800

    output ping result to file, run command by crontab.

--- cisco_get.py
+++ cisco_get.py
@@ -1,22 +1,9 @@
 #!/usr/bin/env python
 
-from netmiko import ConnectHandler
+import os 
 import sys
 
 
 ip = sys.argv[1]
-username = 'netmonitor'
-password = 'Aa12345'
-
-cisco = {'device_type':'cisco_ios', 'ip':ip,'username':username,'password':password} 
-
-net_connect = ConnectHandler(**cisco)
-
-results = net_connect.send_command("ping 114.114.114.114")
-num = 0
-for result in results:
-	if result == '!':
-		num += 1
-
-print num
-#ms = results.split("/")
+ping_file = "/tmp/ping_" + ip
+os.system("cat " + ping_file)
